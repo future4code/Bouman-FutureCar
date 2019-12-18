b4aff7b404693c2f26220d4c8db72ec9fd7a9737
eduardomendesnorberto <eduardomendesnorberto@outlook.com>
2019-12-18 13:52:50 -0300

    teste

--- futurecar/src/App.js
+++ futurecar/src/App.js
@@ -1,54 +1,68 @@
 import React from 'react';
-import styled from "styled-components"
+import styled from 'styled-components';
+
 
 const MainContainer = styled.div `
 
-`
+`;
 
 
 const Header = styled.div `
-display:flex;
-justify-content: center;
-border: solid 1px;
-
-`
+  display:flex;
+  justify-content: center;
+  border: solid 1px;
+`;
 
 const Section = styled.section `
-background-color: #e6e6e6;
-height: 500px;
-padding-top: 50px;
+  background-color: #e6e6e6;
+  height: 500px;
+  padding-top: 70px;
+  margin: 0 auto;
+  
+`;
 
-`
 const Hr = styled.hr `
-padding-top: 50px;
-`
+  padding-top: 50px;
+`;
 
 const Titulo = styled.div `
-width: 100%;
-height: 50px;
-background-color: rgb(255,92,92);
+  width: 100%;
+  height: 50px;
+  background-color: rgb(255,92,92);
+`;
 
-`
-
-
-
-const Footer= styled.div `
-display:flex;
-background-color: rgb(255,92,92);
-height: 50px;
-
-`
+const Footer= styled.footer `
+  display: flex;
+  background-color: rgb(255,92,92);
+  height: 50px;
+  bottom: 0px;
+  position: relative;
+  padding: 5px;
+`;
 
 const ImgFooter= styled.img `
-display:flex;
-width: 40px;
-height: 40px;
-`
+  display:flex;
+  width: 40px;
+  height: 40px;
+`;
+
 const IconeFooter = styled.div `
-`
+`;
 
+const DivButton = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 150px;
+  padding: 10px;
+`;
 
-
+const Button = styled.button `
+  width: 150px;
+  height: 50px;
+  background-color: #ff5c5c;
+  margin-left: 50px;
+`;
 
 
 
@@ -61,16 +75,15 @@ function App() {
      
       <Section>
 
-      <Hr/>
-      
-        <Titulo>
-          FutureCAR VRUMM !
-        </Titulo>
-
-        <button/>       <button/>
-
-
-
+        <Hr/>
+        
+          <Titulo>
+            FutureCAR VRUMM !
+          </Titulo>
+          <DivButton>
+            <Button> Quero Comprar</Button>
+            <Button> Quero Vender</Button>
+          </DivButton>
       </Section>
 
 
