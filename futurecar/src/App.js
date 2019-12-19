268baad2dd9bdec5b2b563dc61ef509f31f45147
Caio-Braga <caioeduardobbsantos@gmail.com>
2019-12-19 17:58:28 -0300

    escrevi o codigo pra buscar os carros da API, ainda falta ajustar algumas coisas

--- futurecar/src/App.js
+++ futurecar/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components"
+import CarrosAPI from './components/grid';
 //import Filtro from './components/Filtro'
 //import Grid from './components/grid'
 
--- futurecar/src/components/grid/index.js
+++ futurecar/src/components/grid/index.js
@@ -2,13 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
+const baseURL = "https://us-central1-future-apis.cloudfunctions.net/futureCar"
 
-const GridCarros = styled.div `
+const CarrosGrid = styled.div `
 display: grid;
 grid-template-columns: 1fr 1fr 1fr;
 
 `
 
+class CarrosAPI extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todosCarros: [],
+        }
+    }
+
+    pegarCarros = async () => {
+        const response = await axios.get(`${baseURL}/cars`)
+        this.setState({todosCarros: response.data.results})
+    }
+
+    componentDidMount(){
+        this.todosCarros();
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+
+
+}
 
 
 
@@ -16,5 +46,4 @@ grid-template-columns: 1fr 1fr 1fr;
 
 
 
-
-export default GridCarros;
+export default CarrosAPI;
