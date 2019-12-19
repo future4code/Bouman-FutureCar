d55335ac9532b56792f074da4c51227c16925e2a
Mariana <coelhomariana@outlook.com>
2019-12-19 13:06:06 -0300

    funcao dos botoes

--- futurecar/src/App.js
+++ futurecar/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import styled from "styled-components"
-import Filtro from './components/Filtro'
+import styled from 'styled-components';
+
 
 const MainContainer = styled.div `
-
 `;
 
 const Header = styled.div `
@@ -26,7 +25,6 @@ const Hr = styled.hr `
 
 const Titulo = styled.div `
   text-align: center;
-
   width: 100%;
   height: 40px;
   background-color: rgb(255,92,92);
@@ -46,8 +44,6 @@ const Test= styled.div`
 display:flex;
 `
 
-const Footer= styled.div `
-
 const ImgFooter= styled.img `
   display:flex;
   width: 40px;
@@ -63,11 +59,6 @@ const IconeFooter = styled.img `
 display:flex;
 width: 40px;
 height: 40px;
-
-`
-const IconeFooter = styled.div `
-`
-
 margin-right: 5px;
 `;
 
@@ -85,45 +76,62 @@ const Button = styled.button `
   margin-left: 50px;
 `;
 
-function App() {
-  return (
-    <MainContainer>
-      <Header>   
-        <img src={require('./components/img/futurecar.png')}  />
-      </Header>
-     
-      <Section>
 
-        <Hr/>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: "" 
+    }
+  }
+
+  handleStore = () => {
+  this.setState({ currentPage: "store" });
+  }
+
+  handleForm = () => {
+  this.setState({ currentPage: "form" });
+  }
+
+  render () {
+    return (
+      <MainContainer>
+        <Header>   
+          <img src={require('./components/img/futurecar.png')}  />
+        </Header>
+      
+        <Section>
+
+          <Hr/>
+          
+            <Titulo>
+            <h2>FutureCAR VRUMM !</h2> 
+            </Titulo>
+            <DivButton>
+              <Button onClick={this.handleStore}> Quero Comprar</Button>
+              <Button onClick={this.handleForm}> Quero Vender</Button>
+              {this.state.currentPage === "store" ? <Store /> : <FormCars />}
+            </DivButton>
+        </Section>
+
+
         
-          <Titulo>
-           <h2>FutureCAR VRUMM !</h2> 
-          </Titulo>
-          <DivButton>
-            <Button> Quero Comprar</Button>
-            <Button> Quero Vender</Button>
-          </DivButton>
-      </Section>
-
-
+        <Footer>
       
-      <Footer>
-    
-      <Test>
-        <ImgFooter src={require('./components/img/carro-preto.png')}/> <h4>FUTURECAR | Going beyond.</h4>
-      </Test>
-        <FooterIcons>
-          <IconeFooter src={require('./components/img/instagram.png')} />
-          <IconeFooter  src={require('./components/img/facebook.png')} />
-          <IconeFooter src={require('./components/img/email.png')}  />
-        </FooterIcons>
-      
-      </Footer>
+        <Test>
+          <ImgFooter src={require('./components/img/carro-preto.png')}/> <h4>FUTURECAR | Going beyond.</h4>
+        </Test>
+          <FooterIcons>
+            <IconeFooter src={require('./components/img/instagram.png')} />
+            <IconeFooter  src={require('./components/img/facebook.png')} />
+            <IconeFooter src={require('./components/img/email.png')}  />
+          </FooterIcons>
+        
+        </Footer>
 
 
     </MainContainer>
-
-  );
+    )}
 }
 
 export default App;
